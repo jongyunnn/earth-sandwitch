7d84e263e2976ca6f06e7b5b6f7249bd24c461c3
grey939597 <jongyunj@icloud.com>
2022-02-23 22:49:30 +0900

    feat: Connect Streaming Sites

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,19 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view />
   </div>
 </template>
 
 <style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap");
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  /* font-family: Avenir, Helvetica, Arial, sans-serif; */
+  font-family: "Roboto", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  width: 100vw;
 }
 
 #nav {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,178 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <header class="header">
+      <img src="/imgs/logo.png" alt="logo" />
+    </header>
+    <div class="img-top">
+      <img src="/imgs/people-white.png" alt="album" />
+    </div>
+    <div class="icons__wrapper">
+      <div class="icon" v-for="(icon, index) in icons" :key="index">
+        <div class="icon-img">
+          <img :src="icon.imgPath" :alt="icon.name" :style="icon.style" />
+        </div>
+        <span class="icon-name">{{ icon.name }}</span>
+      </div>
+    </div>
+    <footer class="footer">
+      <span>Copyright&copy; 2022. Earth Sandwitch. All rights reserved.</span>
+    </footer>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+// import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      icons: [
+        {
+          name: "Apple Music",
+          imgPath: "/imgs/apple.png",
+          to: "",
+          style: "transform: scale3d(0.9, 0.9, 0.9)",
+        },
+        {
+          name: "Spotify",
+          imgPath: "/imgs/spotify.png",
+          to: "",
+          style: "transform: scale3d(0.9, 0.9, 0.9)",
+        },
+        {
+          name: "YouTube",
+          imgPath: "/imgs/youtube.png",
+          to: "",
+          style: "transform: scale3d(0.9, 0.9, 0.9)",
+        },
+        {
+          name: "YT Music",
+          imgPath: "/imgs/ytmusic.png",
+          to: "",
+          style: "",
+        },
+        {
+          name: "Melon",
+          imgPath: "/imgs/melon.png",
+          to: "",
+          style: "",
+        },
+        {
+          name: "Genie",
+          imgPath: "/imgs/genie.png",
+          to: "",
+          style: "",
+        },
+        {
+          name: "Bugs",
+          imgPath: "/imgs/bugs.png",
+          to: "",
+          style: "transform: scale3d(0.9, 0.9, 0.9)",
+        },
+        {
+          name: "Vibe",
+          imgPath: "/imgs/vibe.png",
+          to: "",
+          style: "transform: scale3d(0.9, 0.9, 0.9)",
+        },
+        {
+          name: "Flo",
+          imgPath: "/imgs/flo.png",
+          to: "",
+          style: "transform: scale3d(0.9, 0.9, 0.9)",
+        },
+        {
+          name: "SoundCloud",
+          imgPath: "/imgs/soundcloud.png",
+          to: "",
+          style: "transform: scale3d(0.9, 0.9, 0.9)",
+        },
+      ],
+    };
   },
 };
 </script>
+
+<style lang="scss" scoped>
+* {
+  box-sizing: border-box;
+}
+.home {
+  width: 100%;
+  max-width: 480px;
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  background-color: #fff;
+  box-shadow: 4px 12px 20px 6px rgba(0, 0, 0, 0.18);
+}
+.header {
+  width: 100%;
+  max-width: 480px;
+  color: #ffffff;
+  background-color: #ffffff;
+  z-index: 2;
+  box-shadow: rgba(0, 0, 0, 0.12) 0 2px 16px 0;
+  padding: 8px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  height: 60px;
+  img {
+    width: 65%;
+  }
+}
+.img-top {
+  padding-top: 60px;
+  width: 100%;
+  img {
+    width: 100%;
+  }
+}
+.icons__wrapper {
+  padding: 32px 20px 40px;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  gap: 24px;
+  .icon {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+    align-items: center;
+    width: 100%;
+    height: 22vw;
+    max-height: 120px;
+    .icon-img {
+      box-shadow: 4px 12px 30px 6px rgba(0, 0, 0, 0.09);
+      border-radius: 16px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: 100%;
+      height: 20vw;
+      max-height: 92px;
+      img {
+        width: 100%;
+      }
+    }
+    .icon-name {
+      margin-top: 8px;
+      font-size: 10px;
+      color: #121212;
+    }
+  }
+}
+.footer {
+  display: flex;
+  min-height: 120px;
+  justify-content: center;
+  align-items: center;
+  background-color: #e2e2e2;
+  color: #121212;
+  font-size: 12px;
+}
+</style>
